68430f1c224b068152241d9a782701a0d4b674ab
initkfs <user@user>
2023-07-20 21:34:52 +0300

    Add simple multitasking

--- src/kernel.d
+++ src/kernel.d
@@ -9,36 +9,8 @@ __gshared extern (C)
     size_t _bss_end;
 }
 
-enum taskStackSize = 1024;
-__gshared reg_t[taskStackSize] taskStack;
-
-__gshared RegContext contextCurrent;
-__gshared RegContext contextTask;
-
 import uart;
-
-alias reg_t = size_t;
-
-struct RegContext
-{
-align(1):
-    reg_t ra;
-    reg_t sp;
-
-    //saved
-    reg_t s0;
-    reg_t s1;
-    reg_t s2;
-    reg_t s3;
-    reg_t s4;
-    reg_t s5;
-    reg_t s6;
-    reg_t s7;
-    reg_t s8;
-    reg_t s9;
-    reg_t s10;
-    reg_t s11;
-}
+import task;
 
 extern (C) void dstart()
 {
@@ -51,25 +23,49 @@ extern (C) void dstart()
 
     println("Os start");
 
-    contextTask.ra = cast(reg_t) &userTask;
-    contextTask.sp = cast(reg_t) &taskStack[taskStackSize - 1];
+    taskCreate(&task0);
+    taskCreate(&task1);
 
-    contextSwitch(&contextCurrent, &contextTask);
-
-    println("Os end");
-}
-
-extern (C) void contextSwitch(RegContext* oldContext, RegContext* newContext);
-
-void userTask()
-{
-    println("User task: context switch.");
+    size_t currentTask = 0;
     while (1)
     {
+        println("Run next task.");
+        switchContextToTask(currentTask);
+        println("Back to OS");
+        currentTask = (currentTask + 1) % taskCount;
+        println("---");
+    }
+
+    //println("Os end");
+}
+
+void task0()
+{
+    println("Task0 created.");
+    println("Task0: return to kernel mode.");
+    switchContextToOs();
+    while (true)
+    {
+        println("Task0: running...");
+        delayTicks;
+        switchContextToOs();
     }
 }
 
-void delay(int count = 5000)
+void task1()
+{
+    println("Task1 created.");
+    println("Task1: return to kernel mode.");
+    switchContextToOs();
+    while (true)
+    {
+        println("Task1: running...");
+        delayTicks;
+        switchContextToOs();
+    }
+}
+
+void delayTicks(int count = 5000)
 {
     while (count--)
     {
---
+++ src/task.d
@@ -0,0 +1,63 @@
+/**
+ * Authors: initkfs
+ */
+module task;
+
+enum taskMaxCount = 10;
+enum taskStacksSize = 1024;
+
+__gshared ubyte[taskStacksSize][taskMaxCount] taskStacks;
+__gshared RegContext[taskMaxCount] tasks;
+
+__gshared RegContext contextOs;
+__gshared RegContext* contextCurrent;
+
+__gshared size_t taskCount=0;
+
+alias reg_t = size_t;
+
+struct RegContext
+{
+align(1):
+    reg_t ra;
+    reg_t sp;
+
+    //saved
+    reg_t s0;
+    reg_t s1;
+    reg_t s2;
+    reg_t s3;
+    reg_t s4;
+    reg_t s5;
+    reg_t s6;
+    reg_t s7;
+    reg_t s8;
+    reg_t s9;
+    reg_t s10;
+    reg_t s11;
+}
+
+size_t taskCreate(void function() t)
+{
+	auto i=taskCount++;
+	tasks[i].ra = cast(reg_t) t;
+	tasks[i].sp = cast(reg_t) &taskStacks[i][taskStacksSize-1];
+	return i;
+}
+
+void switchContextToTask(size_t i) {
+	contextCurrent = &tasks[i];
+	contextSwitch(&contextOs, &tasks[i]);
+}
+
+void switchContextToOs() {
+    //back to os
+	RegContext* ctx = contextCurrent;
+	contextCurrent = &contextOs;
+	contextSwitch(ctx, &contextOs);
+}
+
+private {
+    extern (C) void contextSwitch(RegContext* oldContext, RegContext* newContext);
+}
+
